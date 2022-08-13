b03f87ed3b8e2c777370d0c58704b70704556563
Fluto <mesut.l@hotmail.com>
2022-08-13 21:00:45 +1000

    Added support for il2cpp

---
+++ TakoTako/IL2CPPHelper.cs
@@ -0,0 +1,20 @@
+﻿
+#if TAIKO_IL2CPP
+using System;
+using System.Runtime.InteropServices;
+// using Il2CppSystem.Runtime.InteropServices;
+using UnhollowerBaseLib;
+
+namespace TakoTako;
+
+public class ReferenceObject<T> : Il2CppObjectBase
+{
+    public T Value { get; private set; }
+    public ReferenceObject(IntPtr pointer) : base(pointer)
+    {
+        Value = Marshal.PtrToStructure<T>(pointer);
+    }
+    
+    public static  implicit operator T (ReferenceObject<T> value) => value.Value;
+}
+#endif
